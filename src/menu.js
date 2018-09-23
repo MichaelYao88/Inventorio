755fd2e7c506f0fc5febac90a0ff5482d747c4f2
Michael Yao <Michaelyao88@gmail.com>
2018-09-23 20:11:38 +0300

    I think I learned something but not sure. Need to move mutators to parent

--- src/menu.js
+++ src/menu.js
@@ -5,21 +5,23 @@ import './menu.css'
 import {Change} from './xtab.js'
 
 export class Menu extends Component {
+  handleClick() {
+    console.log(this.props.tabs.yo)
+    this.props.tabs.yo=19
+  }
 
-    render() {
-        return (
-            <div className="booty">
-              <button onClick={this.handleClik()}>Current Inventory</button>
-              <button>New Purchase Order</button>
-              <button>Manage Active Orders</button>
-              <button>Usage Report</button>
-              <button>Adjustments Report</button>
-              <div>{this.props.tabs.yo}</div>
-            </div>
-        );
-    }
+  render() {
+      return (
+          <div className="booty">
+            <button onClick={this.handleClick.bind(this)}>Current Inventory</button>
+            <button>New Purchase Order</button>
+            <button>Manage Active Orders</button>
+            <button>Usage Report</button>
+            <button>Adjustments Report</button>
+            <div>{this.props.tabs.yo}</div>
+          </div>
+      );
+  }
 
-    handleClik() {
-      this.props.tabs.yo=9
-    }
+
 }
